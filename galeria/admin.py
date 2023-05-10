b461e47f65e2a88a660bab4f8b1e883fd082dddd
joaocmuniz <joaomuniz.tec@gmail.com>
2023-05-10 15:15:31 -0300

    Conclusão curso Alura ORM com Django

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,10 +3,11 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicado")
     list_display_links = ("id", "nome")
     search_fields = ("nome",) # Este parâmetro deve ser uma tupla, por isso foi adicionada vírgula
     list_filter = ("categoria",)
+    list_editable = ("publicado",)
     list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
